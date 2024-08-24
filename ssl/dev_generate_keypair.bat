feacb158b04832efa87efc8f08ee7ad83cf22885
Krzysztof Czerwiński <krzys.czerwinski@gmail.com>
2024-08-24 11:14:53 +0200

    Updated script generating development keystore to include hostname in subjectAltNames, created script to generate self signed development CA from keystore

--- ssl/dev_generate_keypair.bat
+++ ssl/dev_generate_keypair.bat
@@ -8,7 +8,9 @@ set "KEYALG=EC"
 set "KEYSIZE=256"
 set "SIGALG=SHA256withECDSA"
 set "VALIDITY=365"
-set "CN=localhost"
+set "CN=10.0.2.2"
+@REM SAN - Subject Alternative Name - used for hostname verification to avoid man in the middle attack
+set "SAN=DNS:localhost,IP:10.0.2.2"
 set "OU=Development Team"
 set "O=Company Name"
 set "L=Cracow"
@@ -23,6 +25,7 @@ keytool -keystore "!KEYSTORE!" ^
         -keysize "!KEYSIZE!" ^
         -sigalg "!SIGALG!" ^
         -validity "!VALIDITY!" ^
-        -dname "CN=!CN!, OU=!OU!, O=!O!, L=!L!, ST=!ST!, C=!C!"
+        -dname "CN=!CN!, OU=!OU!, O=!O!, L=!L!, ST=!ST!, C=!C!" ^
+        -ext "SAN=!SAN!"
 
 
